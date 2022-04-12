a441cc49ad3422e8b6de74da19d2be0254481501
Maxim Kochurov <max.2505@yandex.ru>
2022-04-12 19:10:23 +0300

    Релиз

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+const editBtn = document.querySelector('.profile__edit-button');
+const popupCloseBtn = document.querySelector('.popup__close-button');
+const popup = document.querySelector('.popup');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+const popupName = document.querySelector('.popup__form_el_name');
+const popupDescription = document.querySelector('.popup__form_el_description');
+const popupSubmitBtn = document.querySelector('.popup__submit-button');
+
+
+popupName.value = profileName.textContent;
+popupDescription.value = profileDescription.textContent;
+
+
+function changeName (){
+    profileName.textContent = popupName.value;
+    profileDescription.textContent = popupDescription.value;
+    popupToggle()
+}
+
+function popupToggle() {
+    popup.classList.toggle('popup_opened')
+}
+
+function popupOverlayClick(evt) {
+    if (evt.target === evt.currentTarget){
+        popup.classList.toggle('popup_opened')  
+    }
+}
+
+
+editBtn.addEventListener('click', popupToggle);
+popupCloseBtn.addEventListener('click', popupToggle);
+popup.addEventListener('click', popupOverlayClick)
+popupSubmitBtn.addEventListener('click', changeName)
